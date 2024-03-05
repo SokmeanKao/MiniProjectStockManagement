844954055208281d456d0c175c2b894fc5624798
Manin1903 <sokmanin.1918@gmail.com>
2024-03-05 14:34:17 +0700

    Test Commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         StockController sc = new StockController();
         sc.getProducts();
+        System.out.println("Hello world");
     }
 
 
